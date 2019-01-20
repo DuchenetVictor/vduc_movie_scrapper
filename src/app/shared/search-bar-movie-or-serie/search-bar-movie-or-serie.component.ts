70966ab22aaf63f65a4ba996c503e90b89196eb7
victor <victor.duchenet@ynov.com>
2019-01-20 16:16:37 +0100

    fix shared module, add search and click

--- src/app/shared/search-bar-movie-or-serie/search-bar-movie-or-serie.component.ts
+++ src/app/shared/search-bar-movie-or-serie/search-bar-movie-or-serie.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { mediaDetail } from './../../models/mediaDetail';
+import { RestApiService } from './../../services/rest-api.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { mediaTypeEnum } from 'src/app/models/mediaTypeEnum';
+import { searchMedia } from 'src/app/models/searchMedia';
 
 @Component({
   selector: 'app-search-bar-movie-or-serie',
@@ -6,10 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar-movie-or-serie.component.scss']
 })
 export class SearchBarMovieOrSerieComponent implements OnInit {
+  @Input() mediaType: mediaTypeEnum;
+  public mediaDetails: mediaDetail[];
 
-  constructor() { }
+  constructor(private router: Router, private restApiService: RestApiService) {
+    this.initializeItems();
+  }
 
   ngOnInit() {
   }
+  initializeItems() {
+    this.mediaDetails = [];
+  }
 
+  getItems(env: any) {
+    this.initializeItems();
+    const value = env.target.value;
+
+    if (value && value.trim() != '') {
+      this.restApiService.getMedias<searchMedia>(value, this.mediaType).subscribe(
+        data => {
+          if (data != null) {
+            this.mediaDetails = data.Search;
+          }
+        },
+        err => {
+          console.log(err);
+        });
+    }
+  }
+  getMedia(mediaDetail : mediaDetail){
+    if (this.mediaType === mediaTypeEnum.movie) {
+      this.router.navigateByUrl("/movie-detail?param=" + mediaDetail.imdbID); //TODO: bonne route ! 
+    }
+    if (this.mediaType === mediaTypeEnum.serie) {
+      this.router.navigateByUrl("/serie-details?param=" + mediaDetail.imdbID);//TODO bonne route :
+    }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarMovieOrSerieComponent } from './search-bar-movie-or-serie/search-bar-movie-or-serie.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [SearchBarMovieOrSerieComponent],
   exports: [SearchBarMovieOrSerieComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class SharedModule { }
