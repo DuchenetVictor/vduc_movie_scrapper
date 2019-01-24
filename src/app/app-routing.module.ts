d991a1b04dd1977854b8ea2336957776a1393b29
victor <victor.duchenet@ynov.com>
2019-01-24 14:37:29 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// const routes: Routes = [
-//   { path: '', loadChildren: './tabs/tabs/tabs.module#TabsPageModule' },
-//   { path: 'tabs/movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
-//   { path: 'tabs/series', loadChildren: './pages/series/series.module#SeriesPageModule' },
-//   { path: 'media-details', loadChildren: './pages/media-details/media-details.module#MediaDetailsPageModule' },
-//   { path: 'season-details', loadChildren: './pages/season-details/season-details.module#SeasonDetailsPageModule' }
-// ];
-
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs/tabs.module#TabsPageModule'},
+  { path: '', redirectTo:'tabs/movies', pathMatch:'prefix'},
+  { path :'tabs', loadChildren: './tabs/tabs/tabs.module#TabsPageModule'},
   { path: 'media-details', loadChildren: './pages/media-details/media-details.module#MediaDetailsPageModule' },
   { path: 'season-details', loadChildren: './pages/season-details/season-details.module#SeasonDetailsPageModule' }
 ];
