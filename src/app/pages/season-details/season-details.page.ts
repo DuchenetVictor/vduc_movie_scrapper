c4ac3ad756b873181622bdb26e9fa017fd897017
DuchenetVictor <victor.duchenet@ynov.com>
2019-03-22 10:43:38 +0100

    add some info for season-detail

--- src/app/pages/season-details/season-details.page.ts
+++ src/app/pages/season-details/season-details.page.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { RestApiService } from 'src/app/services/rest-api/rest-api.service';
-import { MediaTypeEnum } from 'src/app/models/mediaTypeEnum';
-import { EpisodeResume } from 'src/app/models/episodeResume';
-import { Episode } from 'src/app/models/episode';
-import { ActivatedRoute } from '@angular/router';
+import { MediaDetail } from "src/app/models/mediaDetail";
+import { Component, OnInit } from "@angular/core";
+import { RestApiService } from "src/app/services/rest-api/rest-api.service";
+import { MediaTypeEnum } from "src/app/models/mediaTypeEnum";
+import { EpisodeResume } from "src/app/models/episodeResume";
+import { Episode } from "src/app/models/episode";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-season-details',
-  templateUrl: './season-details.page.html',
-  styleUrls: ['./season-details.page.scss'],
+  selector: "app-season-details",
+  templateUrl: "./season-details.page.html",
+  styleUrls: ["./season-details.page.scss"]
 })
 export class SeasonDetailsPage implements OnInit {
-
-  constructor(private activatedRoute: ActivatedRoute, private restApi: RestApiService) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private restApi: RestApiService,
+    private router: Router,
+    private location: Location
+  ) {
     this.activatedRoute.queryParams.subscribe(params => {
-      this.imdb = params['imdb'];
-      this.seasonNbr = params['seasonNbr'];
+      this.imdb = params["imdb"];
+      this.seasonNbr = params["seasonNbr"];
     });
   }
 
@@ -25,17 +31,24 @@ export class SeasonDetailsPage implements OnInit {
   public episodes: Episode[];
 
   ngOnInit() {
-
-
-    this.restApi.getMedia<EpisodeResume>(this.imdb, MediaTypeEnum.episode, this.seasonNbr).subscribe(
-      (res) => {
-        this.episodeResume = res;
-        this.episodes = res.Episodes;
-      },
-      (err) => {
-        console.error(err);
-      }
-    );
+    this.restApi
+      .getMedia<EpisodeResume>(this.imdb, MediaTypeEnum.episode, this.seasonNbr)
+      .subscribe(
+        res => {
+          this.episodeResume = res;
+          this.episodes = res.Episodes;
+        },
+        err => {
+          console.error(err);
+        }
+      );
   }
 
+  showEpisode(mediaDetail: MediaDetail) {
+    this.router.navigateByUrl("/media-details?param=" + mediaDetail.imdbID);
+  }
+
+  comeBack() {
+    this.location.back();
+  }
 }
